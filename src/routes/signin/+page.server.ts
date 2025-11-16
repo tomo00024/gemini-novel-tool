1b551cb1f11f856557ce77e88b004b57fef78aef
tomo <asds7000@gmail.com>
2025-11-16 17:08:52 +0900

    test

--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,4 +1,4 @@
 import { signIn } from '../../auth';
 import type { Actions } from './$types';
-
+export const prerender = false;
 export const actions: Actions = { default: signIn };
--- src/routes/signout/+page.server.ts
+++ src/routes/signout/+page.server.ts
@@ -1,4 +1,4 @@
 import { signOut } from '../../auth';
 import type { Actions } from './$types';
-
+export const prerender = false;
 export const actions: Actions = { default: signOut };
